a2ddbed0c446b433309b92a83307f407acd983f4
Ralfe Reis <ralfe.cavalcante@gmail.com>
2020-04-28 23:14:04 -0300

    Authentication and Images Upload Classes finished

--- src/database/migrations/1587683382003-CreateAppointments.ts
+++ src/database/migrations/1587683382003-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587683382003
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
