17601025990db6d364eb9874d960803b2521c3bd
Ralfe Reis <ralfe.cavalcante@gmail.com>
2020-12-20 23:07:01 -0300

    Leading with the error

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '4c76138ea0fb8312a37e1fa6277a8c3c',
+        expiresIn: '1d',
+    },
+};
